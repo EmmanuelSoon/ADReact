5501135df0dcc1469c48a05f5c10697d48d6de8a
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-02 16:37:51 +0800

    recipe initial template

--- src/Components/RecipeComponent.js
+++ src/Components/RecipeComponent.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import { withRouter } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import {Container, Row, Col, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 function Recipe(){
 
@@ -16,11 +17,78 @@ function Recipe(){
     const [recipe, setRecipe] = useState(emptyRecipe);
 
     return (
-        <div>
-            <h1>{recipe.dish.name}</h1>
-            {/* add the details, steps and nutritional values */}
+        <div className='container'>
+
+        <div className="row mt-5 mb-5" >
+            <div className="col-12 col-md-8">
+                <div className="my-5">
+                    <span>April 05, 2018</span>
+                    <h2>Vegetarian cheese salad</h2>
+                    <div >
+                        <h6>Prep: 15 mins</h6>
+                        <h6>Cook: 30 mins</h6>
+                        <h6>Yields: 8 Servings</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4">
+                        <div class="text-right my-5">
+                            <div class="ratings">
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star" aria-hidden="true"></i>
+                                <i class="fa fa-star-o" aria-hidden="true"></i>
+                            </div>
+                            <button className='btn btn-primary'>Add rating</button>
+                        </div>
+                    </div>
         </div>
+        <div className="row d-flex justify-content-center" >
+            <div className='col-auto'>
+                <Image alt='placeholder' src= {process.env.PUBLIC_URL + '/asset/images/placeholder.jpg'} width="600" height='500'></Image>
+            </div>
+        </div>
+        <div className='row d-flex justify-content-around mt-5'>
+            <div className='col-4 offset-1 align-middle'>
+                <div>
+                    <h3>Ingredients</h3>
+                </div>
+                <ol>
+                    <li>ingredient 1</li>
+                    <li>ingredient 1</li>
+                    <li>ingredient 1</li>
+                    <li>ingredient 1</li>
+                </ol>
+            </div>
+            <div className='col-4 align-middle'>
+                <div>
+                    <h3>Nutritional Value</h3>
+                    <ol>
+                        <li>ingredient 1: 100 cal</li>
+                        <li>ingredient 1: 100 cal</li>
+                        <li>ingredient 1: 100 cal</li>
+                        <li>ingredient 1: 100 cal</li>
+                    </ol>
+                </div>
+            </div>
+            <div className='row d-flex justify-content-center mt-5 pb-5 px-5'>
+                <div className='col-9 border p-3'>
+                    <h2 className= "mb-5">Procedure</h2>
+                    <div>
+                        step 1: asdasdasdasdadada. <br/>
+                        step 2: asasdasdadasdadad. <br/>
+                        step 3: adsdadadadadadadad. <br/>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+      </div>
+
+
     )
 }
 
-export default withRouter(Recipe); 
+export default Recipe; 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'font-awesome/css/font-awesome.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
