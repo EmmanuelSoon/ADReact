78eda84b83887f6145b03610f2b160ad70b0412e
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-08-04 20:16:27 +0800

    edit recipe

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -4,6 +4,7 @@ import {Routes , Route, Navigate, useSearchParams } from 'react-router-dom';
 import Home from './HomeComponent'
 import Recipe from './RecipeComponent'
 import Header from './HeaderComponent'
+import EditRecipe from './NewRecipeForm/EditRecipe'
 
 function Main () {
 
@@ -18,7 +19,10 @@ function Main () {
             <Header />
             <Routes>
                 <Route path="/" element={<Home userId={searchParams.get("userId")}/>}/>
-                <Route path="/recipes/:id" element={<Recipe />} />
+                <Route path="/recipes/:id" element={<Recipe userId={searchParams.get("userId")}/>} />
+                
+                <Route path="/recipe/edit/:id" element={<EditRecipe userId={searchParams.get("userId")}/>} />
+
 
                 <Route path="*" element= {<Navigate to="/" replace />} /> 
             </Routes>
