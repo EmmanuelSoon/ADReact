773c7fbe11245d6721cdec88d99dea06fe95905d
lyc <280369571@qq.com>
2022-08-05 10:41:42 +0800

    Login and added local storage

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -4,26 +4,28 @@ import {Routes , Route, Navigate, useSearchParams } from 'react-router-dom';
 import Home from './HomeComponent'
 import Recipe from './RecipeComponent'
 import Header from './HeaderComponent'
-
+import NewRecipe from './NewRecipeForm/NewRecipe';
+import Login from './Login/Login';
+import LoginWithData from './Login/LoginWithData';
 function Main () {
 
     const [searchParams, setSearchParams] = useSearchParams();
-
     // useEffect(()=>{
     //     console.log(searchParams.get("userId"))
     // },[searchParams])
-
+    // const {userId} = searchParams.get("userId")
     return (
         <div>
-            <Header />
+            <Header/>
             <Routes>
-                <Route path="/" element={<Home userId={searchParams.get("userId")}/>}/>
-                <Route path="/recipes/:id" element={<Recipe />} />
-
-                <Route path="*" element= {<Navigate to="/" replace />} /> 
+                {/* <Route path="/" element={<Home userId={searchParams.get("userId")}/>}/>
+                <Route path="/recipes/:id" element={<Recipe />} /> */}
+                <Route path='/recipe/new' element={<NewRecipe />} />
+                <Route path='/login' element={<Login />} />
+                {/* <Route path='/login/:username/:password' element={<LoginWithData />} />
+                <Route path="*" element= {<Navigate to="/" replace />} />  */}
             </Routes>
         </div>
     )
 }
-
 export default Main;
